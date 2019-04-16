deb513bb8ca899b148941c2410840e1ca679417c
FredSt0ne <fredstone624@gmail.com>
2019-04-16 15:35:44 +0300

    finished AsyncIO tasks

--- 08-AsyncIO/AsyncIO/Tasks.cs
+++ 08-AsyncIO/AsyncIO/Tasks.cs
@@ -3,15 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncIO
 {
     public static class Tasks
     {
-
-
         /// <summary>
         /// Returns the content of required uris.
         /// Method has to use the synchronous way and can be used to compare the performace of sync \ async approaches. 
@@ -21,9 +18,11 @@ namespace AsyncIO
         public static IEnumerable<string> GetUrlContent(this IEnumerable<Uri> uris) 
         {
             // TODO : Implement GetUrlContent
-            throw new NotImplementedException();
+            using (var client = new WebClient())
+            {
+                return uris.Select(x => client.DownloadString(x));
+            }
         }
-
 
 
         /// <summary>
@@ -38,7 +37,23 @@ namespace AsyncIO
         public static IEnumerable<string> GetUrlContentAsync(this IEnumerable<Uri> uris, int maxConcurrentStreams)
         {
             // TODO : Implement GetUrlContentAsync
-            throw new NotImplementedException();
+            using (var client = new WebClient())
+            {
+                var enumer = uris.GetEnumerator();
+                var tasks = new List<Task<string>>(uris.Take(maxConcurrentStreams).Select(x => client.DownloadStringTaskAsync(x)));
+
+                while (tasks.Count > 0)
+                {
+                    yield return tasks.First().Result;
+
+                    tasks.RemoveAt(0);
+
+                    if (enumer.MoveNext())
+                    {
+                        tasks.Add(client.DownloadStringTaskAsync(enumer.Current));
+                    }
+                }
+            }
         }
 
 
@@ -50,14 +65,37 @@ namespace AsyncIO
         /// </summary>
         /// <param name="resource">Uri of resource</param>
         /// <returns>MD5 hash</returns>
-        public static Task<string> GetMD5Async(this Uri resource)
+        public static async Task<string> GetMD5Async(this Uri resource)
         {
             // TODO : Implement GetMD5Async
-            throw new NotImplementedException();
+            
+
+            // Read data in parts
+            using (var client = new WebClient())
+            using (var stream = await client.OpenReadTaskAsync(resource))
+            {
+                var md5 = MD5.Create();
+                var buffer = new byte[2048];
+                var bytes = 0;
+
+                while ((bytes = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    md5.TransformBlock(buffer, 0, bytes, buffer, 0);
+                }
+
+                md5.TransformFinalBlock(new byte[0], 0, 0);
+
+                return string.Concat(md5.Hash.Select(x => x.ToString("X2")));
+            }
+
+
+            // Read full data
+            //using (var client = new WebClient())
+            //{
+            //    var data = await client.DownloadDataTaskAsync(resource);
+
+            //    return string.Concat(MD5.Create().ComputeHash(data).Select(x => x.ToString("X2")));
+            //}
         }
-
     }
-
-
-
-}
+}         
